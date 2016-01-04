34af0f82468e25fb365093d0ac3a485f2256e3c5
GFROCRAI <gwenda.frocrain@gmail.com>
2016-01-04 21:24:11 +0100

    add console.log

--- source/javascripts/smooth_scroll.js
+++ source/javascripts/smooth_scroll.js
@@ -1,7 +1,10 @@
 
 $(function() {
   // Smooth scroll
+  console.log('initialisation de smoothy');
+
   $('a[href*=#]:not([href=#])').click(function() {
+    console.log('click called', this);
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
